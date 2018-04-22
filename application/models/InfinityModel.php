a886074d0832417857db5045adcfeac68a97de2d
Shahnewaz Ahmed <shahnewaz24557@gmail.com>
2018-04-22 15:44:49 +0600

    Update InfinityModel.php

--- application/models/InfinityModel.php
+++ application/models/InfinityModel.php
@@ -1,9 +1,3 @@
-<!--
- * InfinityScroll v1.1 (https://github.com/shahnewaz24557/ci-ajax-infinity-scroll)
- * Copyright (c) 2018 Shahnewaz Ahmed
- * Licensed under MIT (https://github.com/shahnewaz24557/ci-ajax-infinity-scroll/blob/master/LICENSE)
- -->
- 
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
