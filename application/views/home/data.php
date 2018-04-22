b1efd32e9b51c545987223d2d57c6c2897725528
Shahnewaz Ahmed <shahnewaz24557@gmail.com>
2018-04-22 15:46:36 +0600

    Update data.php

--- application/views/home/data.php
+++ application/views/home/data.php
@@ -1,9 +1,3 @@
-<!--
- * InfinityScroll v1.1 (https://github.com/shahnewaz24557/ci-ajax-infinity-scroll)
- * Copyright (c) 2018 Shahnewaz Ahmed
- * Licensed under MIT (https://github.com/shahnewaz24557/ci-ajax-infinity-scroll/blob/master/LICENSE)
- -->
-
 <?php foreach($posts as $post){ ?>
 <div>
 	<h3><a href=""><?php echo $post->title ?></a></h3>
